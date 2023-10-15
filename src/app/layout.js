41be050a18dcc09786b405934ae9ead7ee292151
Sanjampreet15 <dhanjusanjam@gmail.com>
2023-10-16 00:07:34 +0530

    UI code

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,30 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
+import { ThemeContexProvider } from "@/context/ThemeContext";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Blog App',
-  description: 'The best blog app!',
-}
+  title: "Blog App",
+  description: "The best blog app!",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeContexProvider>
+        <div className="container">
+          <div className="wrapper">
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
+        </div>
+        </ThemeContexProvider>
+      </body>
     </html>
-  )
+  );
 }
