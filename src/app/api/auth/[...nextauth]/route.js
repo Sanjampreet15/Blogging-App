b53f281a58d3bcf74c26806b11139c280fd1620d
Sanjampreet15 <dhanjusanjam@gmail.com>
2023-10-22 15:31:51 +0530

    Done authentication using console.cloud

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,1 +1,7 @@
 // This file  contain the code for API integration
+import { authOptions } from "@/utils/auth"
+import NextAuth from "next-auth"
+
+const handler = NextAuth(authOptions); 
+
+export {handler as GET, handler as  POST};
