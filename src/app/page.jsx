9d711434c7c0a13eab22d21879703ce8d1dab058
Sanjampreet15 <dhanjusanjam@gmail.com>
2023-10-23 15:28:45 +0530

    Last part of the project

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,21 @@
-import Navbar from "@/components/navbar/Navbar";
-import styles from "./homepage.module.css";
 import Link from "next/link";
-import Footer from "@/components/footer/Footer";
+import styles from "./homepage.module.css";
 import Featured from "@/components/featured/Featured";
 import CategoryList from "@/components/categoryList/CategoryList";
 import CardList from "@/components/cardList/CardList";
-import Menu from "@/components/menu/Menu";
+import Menu from "@/components/Menu/Menu";
 
-export default function Home() {
-  return  ( 
-  <div className={styles.container}>
-    <Featured/>
-    <CategoryList/>
-    <div className={styles.content}></div>
-     <CardList/>
-     <Menu/>
-</div>
-  ); 
+export default function Home({ searchParams }) {
+  const page = parseInt(searchParams.page) || 1;
+
+  return (
+    <div className={styles.container}>
+      <Featured />
+      <CategoryList />
+      <div className={styles.content}>
+        <CardList page={page}/>
+        <Menu />
+      </div>
+    </div>
+  );
 }
